8666c387e74b4fcf0630762c6b22f7359b5029f1
Kate <katerebekah@gmail.com>
2015-05-11 18:51:54 -0500

    setting up mongolab in heroku

--- movieSecret/db.js
+++ movieSecret/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/secret');
+//mongoose.connect('mongodb://localhost/secret');
+mongoose.connect(process.env.DB);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
